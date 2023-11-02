cc3a9c869388a59676561ddf6afc1c4beeb59233
Dmitry <112936821+DmitryGagarin@users.noreply.github.com>
2023-11-02 21:36:18 +0300

    new attempt of using API + navigationLink

---
+++ EASY-PET/GetYourBeer/GetYourBeer/GetYourBeerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetYourBeerApp.swift
+//  GetYourBeer
+//
+//  Created by Dmitry Fatsievich on 02.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct GetYourBeerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
